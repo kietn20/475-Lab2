cddfc55dc08d636ae453e67c13fb255ae5b290df
kietn20 <kietnguyen3698@gmail.com>
2024-10-21 21:58:02 -0700

    started

---
+++ Lab 2/Data/TodoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models; // Ensure this is the namespace where your model resides
+
+namespace TodoApi.Data;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options): base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+}
